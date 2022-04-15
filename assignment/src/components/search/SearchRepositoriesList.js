8a5c718afcbd96996a414cc184ffa4775fa8d70d
awdsza <37355034+awdsza@users.noreply.github.com>
2022-04-15 16:27:26 +0900

    이주헌 - 저장소 중복 저장 방지 기능 추가

--- assignment/src/components/search/SearchRepositoriesList.js
+++ assignment/src/components/search/SearchRepositoriesList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {setSelectedRepo,countSelectedRepo,getSelectedRepo} from '../../storage/storage.js';
 import {ADD_REPOSITORY_LIMIT} from '../../utils/constants.js';
-import {ADD_REPOSITORY_LIMIT_WARN} from '../../utils/keywords.js';
+import {ADD_REPOSITORY_LIMIT_WARN,ALERT_DUPLICATE_ADD_REPO,ALERT_ADD_REPO} from '../../utils/keywords.js';
 
 import Pager from '../common/Pager';
 
@@ -15,10 +15,14 @@ function SearchRepositoriesList({repositories,pager,onClickPage}){
             }
 
             const selectedRepo = getSelectedRepo();
+            if(selectedRepo.some( _repo => _repo.id === repo.id )){
+                alert(ALERT_DUPLICATE_ADD_REPO);
+                return;
+            }
             selectedRepo.push(repo);
     
             setSelectedRepo(selectedRepo);
-        
+            alert(ALERT_ADD_REPO);
     }
 
     return (
--- assignment/src/utils/keywords.js
+++ assignment/src/utils/keywords.js
@@ -1,4 +1,5 @@
 import {ADD_REPOSITORY_LIMIT} from './constants.js'
 const ADD_REPOSITORY_LIMIT_WARN = `Public Repository 등록개수는 최대 ${ADD_REPOSITORY_LIMIT}개 입니다.`;
-
-export {ADD_REPOSITORY_LIMIT_WARN};
+const ALERT_DUPLICATE_ADD_REPO = '이미 추가된 저장소입니다.';
+const ALERT_ADD_REPO = '추가 되었습니다.'
+export {ADD_REPOSITORY_LIMIT_WARN,ALERT_DUPLICATE_ADD_REPO,ALERT_ADD_REPO};
